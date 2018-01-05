ab14ec49ed3e55856ac2b0d16a89441783469d45
Andrew Dean <andrewdean565@gmail.com>
2018-01-05 18:08:56 +0000

    adding vue to render list

--- src/js/app.js
+++ src/js/app.js
@@ -11,12 +11,37 @@ var app = new Vue({
             name : "bananas",
             amount : 2,
             price : 1.99
+          },
+          {
+            name : "apples",
+            amount : 2,
+            price : 1.87
           }
         ]
       }
     ],
-    active : 0
-  }
+    active : 0,
+    editItem : {
+      name : "",
+      amount : 1,
+      price : 0.00,
+    },
+    limit : 5
+  },
+  methods: {
+    totalCost: function (items) {
+      var total = 0;
+      items.forEach(function(item) {
+        total += (item.price * item.amount);
+      });
+      return Math.round(total * 100) / 100;
+    },
+    addItem: function (item) {
+      lists[active].items.push(item);
+
+    }
+  },
+
 })
 
 Vue.component('shop-item', {
